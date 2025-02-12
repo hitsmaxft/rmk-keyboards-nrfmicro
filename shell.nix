4a686d87f6203b4e3e3dd3169957f8427f365974
麒翔 <qixiang.mft@taobao.com>
2025-02-12 17:10:04 +0800

    init

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  system = pkgs.system;
+  systemBuildInputs = system: pkgs:
+    [ pkgs.iconv pkgs.openssl ]
+    ++ (if pkgs.lib.strings.hasInfix "$system" "darwin" then [
+      pkgs.darwin.apple_sdk.frameworks.Security
+      pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+    ] else [ ]);
+in with pkgs;
+mkShell {
+  shellHook = "export PATH=${"$"}PATH:~/.cargo/bin;";
+  buildInputs = [
+    rustup
+    rustfmt
+    cargo-make
+    probe-rs-tools
+    minicom
+    #nrf-command-line-tools
+    # pre-commit
+    # rustPackages.clippy
+  ] ++ (systemBuildInputs system pkgs);
+  RUST_SRC_PATH = rustPlatform.rustLibSrc;
+}
+
